2c6f1cdadb1b280f35bc05cc6289d32f04909be9
Robmoi <93385693+Robmoi@users.noreply.github.com>
2023-03-07 13:08:55 +0100

    Update Person.java

--- assignment-oop-files/Person.java
+++ assignment-oop-files/Person.java
@@ -3,13 +3,28 @@ public class Person implements Serializable{
   private static final long serialVersionUID = 3487495895819393L;
   private String name;
   private String email;
+  private String phone;
+ 
+  public Person(String name){
+	  if (name == null) {
+		  throw new NullPointerException();
+	    }
+	  this.name = name;
+	  }
+	  public Person(String name, String email){
+		this.email = email;
+		this(name);
+	  }
+	  public Person(String name, String email, String phone){
+		this(name,email);
+		this.phone = phone;
+	  }
+	  public String name() {return name; }
+	  public String email(){return email;}
+	  public String phone(){return phone;}
 
-  public Person(String name, String email){
-    this.name = name;
-    this.email= email;
-  }
-  @Override
-  public String toString(){
-    return String.format("%s - %s", name, email);
-  }
-}
+	  @Override
+	  public String toString(){
+	    return String.format("%s  %s  %s", name, (email==null?"":email), (phone==null?"":phone));
+	  }
+	}
